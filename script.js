d99cf817d5ff7781476d7642661ddf3fbf5ba46a
Nastya Boiko <killer99whale@mail.ru>
2023-03-08 14:22:03 +0300

    Without 250 metres

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getRandomInt(min, max) {
+Math.getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 // Задача 1
@@ -50,7 +50,7 @@ let charecter;
 
 let timer = setTimeout(go = () => {
     
-    charecter = (i == 0) ? 0 : getRandomInt(-15, 15);
+    charecter = (i == 0) ? 0 : Math.getRandom(-15, 15);
 
     schedule.setHours(schedule.getHours() + stops[i].time);
     
@@ -58,7 +58,7 @@ let timer = setTimeout(go = () => {
 
     date.setMinutes(date.getMinutes() + charecter);
 
-    console.log(`${charecter} : ${stops[i].name}, время: ${date.toLocaleTimeString()}, ожидаемое время: ${schedule.toLocaleTimeString()}, характер: ${defineCharecter(schedule, date)}; ${(new Date).getMinutes()}:${(new Date).getSeconds()}:${(new Date).getMilliseconds()}`);
+    // console.log(`${charecter} : ${stops[i].name}, время: ${date.toLocaleTimeString()}, ожидаемое время: ${schedule.toLocaleTimeString()}, характер: ${defineCharecter(schedule, date)}; ${(new Date).getMinutes()}:${(new Date).getSeconds()}:${(new Date).getMilliseconds()}`);
     i++;
     if (i < stops.length) {
         setTimeout(go, stops[i].time * 1200 + charecter * 1200 / 60)
@@ -68,11 +68,49 @@ let timer = setTimeout(go = () => {
 //Задача 2
 // Временной масштаб симуляции: 320мс:1ч.
 
-distance = 40
+function Turtle(name) {
+    this.name = name;
+    this.distance = 40;
+    this.velocity = 2;
+    this.timeAll = 0;
+    this.timeLim = 4;
+    this.sleep = Math.getRandom(3, 5);
 
-velocity = 2
+    this.changeSleep = () => {
+        this.sleep = Math.getRandom(3, 5);
+    }
 
-timeLim = 4
+    this.run = () => {
+        let length = this.velocity * this.timeLim
+        this.timeAll += this.timeLim
+        this.distance -= length
+        console.log(`Черепаха ${this.name} преодолевает ${length} км за ${this.timeLim} часа, осталось: ${this.distance} км\n`)
+        Boolean(this.distance)? setTimeout(this.rest, this.timeLim * 320) : ``;
+    }
+
+    this.rest = () => {
+        this.changeSleep()
+        this.timeAll += this.sleep;
+        console.log(`Черепаха ${this.name} устала и спит ${this.sleep} часа\n`)
+        Boolean(this.distance)? setTimeout(this.run, this.sleep * 320) : ``;
+    }
+
+    this.zabeg = () => {
+        setTimeout(this.run, this.timeLim * 320);
+    }
+
+
+    this.timeAllF = () => this.timeAll
+
+
+
+}
+
+let paha = new Turtle('Paha')
+let paha2 = new Turtle('Paha2')
+paha.zabeg();
+paha2.zabeg();
+
 
 rest = 4 //3-5
 
