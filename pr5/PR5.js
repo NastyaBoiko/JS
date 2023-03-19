c671e7928e0f7e4841c5270cc5d15397d4d84db0
Nastya Boiko <killer99whale@mail.ru>
2023-03-19 15:34:36 +0300

    made console.log

--- pr5/PR5.js
+++ pr5/PR5.js
@@ -60,28 +60,39 @@ class ErrorType extends Error
     }
 }
 
-function fact (num) {
-    let res
-    if (typeof num == 'number') {
-        if (num > 1) {
-            res = num * fact(num - 1)
-        } else if (num < 0) { 
-            throw new ErrorMinus(`${num} меньше 0!`)
-        } else {
-            res = 1
+class Fact {
+
+    fact(num) {
+        let res
+        try {
+            if (typeof num == 'number') {
+                if (num > 1) {
+                    res = num * this.fact(num - 1)
+                } else if (num < 0) { 
+                    throw new ErrorMinus(`${num} меньше 0!`)
+                } else {
+                    res = 1
+                }
+            } else {
+                throw new ErrorLetter(`${num} не является числом!`)
+            }
+            
+        } catch (err) {
+            res = err.myMessage
         }
-    } else {
-        throw new ErrorLetter(`${num} не является числом!`)
+        
+        return res
     }
-    return res
 }
 
-try {
-    // fact(-1)
-    // fact('a12')
+facty = new Fact();
+
+/* try {
+    console.log(facty.fact(0))
+    // console.log(facty.fact('a'))
 } catch (err) {
     console.log(err.myMessage)
-}
+} */
 
 /* 
 б) реализовать класс, находящий корни квадратного уравнения. Вызов метода
@@ -106,52 +117,65 @@ class Square
         }
     }
 
-    sqrt() {
-        let res;
-        let D;
-        let x;
-        let x1;
-        let x2;
-
-        if (this.checkZero){
-            throw new ErrorValues('введены все 0!')
-        } else if (this.check) {
-            if (this.#a != 0) {
-                if(this.#b == 0 && this.#c == 0) {
-                    res = "0"
-                } else if (this.#b == 0 && this.#a / this.#c < 0) {
-                    x1 = (-this.#c / this.#a) ** 0.5
-                    x2 = -((-this.#c / this.#a) ** 0.5)
-                    res = `${x1}; ${x2}`
-                } else if (this.#b == 0 && this.#a / this.#c > 0) {
-                    res = "корней нет"
-                } else if (this.#c == 0) {
-                    x1 = 0
-                    x2 = -(this.#b / this.#a)
-                    res = `${x1}; ${x2}`
-                } else if (this.#b != 0 && this.#c != 0) {
-                    D = this.#b ** 2 - 4 * this.#a * this.#c
-                    if (D > 0) {
-                        x1 = (-this.#b + D ** 0.5) / (2 * this.#a)
-                        x2 = (-this.#b - D ** 0.5) / (2 * this.#a)
-                        res = `${x1}; ${x2}`
-                    } else if (D == 0) {
-                        x = (-this.#b + D ** 0.5) / (2 * this.#a)
-                        res = `${x}`
-                    } else {
-                        res = 'корней нет'
-                    }
-                }
-            } else if (this.#b == 0 && this.#c != 0) {
-                res = "корней нет"
-            } else {
-                // x = -(this.#c / this.#b)
-                // res = `${x}`
-                throw new ErrorType('Введено не квадратное уравнение, а линейное!')
+    sqrt(...mas) {
+        this.#a = mas[0];
+        this.#b = mas[1];
+        this.#c = mas[2];
+        try {
+            let res;
+            let D;
+            let x;
+            let x1;
+            let x2;
+    
+            if (mas.length > 3) {
+                throw new ErrorType('Слишком много аргументов!')
+            } else if (mas.length == 0) {
+                throw new ErrorType('Слишком мало аргументов!')
             }
-            return `Ответ: ${res}`;
-        } else {
-            throw new ErrorLetter('в параметрах не все числа!')
+
+            if (this.checkZero){
+                throw new ErrorValues('введены все 0!')
+            } else if (this.check) {
+                if (this.#a != 0) {
+                    if(this.#b == 0 && this.#c == 0) {
+                        res = "0"
+                    } else if (this.#b == 0 && this.#a / this.#c < 0) {
+                        x1 = (-this.#c / this.#a) ** 0.5
+                        x2 = -((-this.#c / this.#a) ** 0.5)
+                        res = `${x1}; ${x2}`
+                    } else if (this.#b == 0 && this.#a / this.#c > 0) {
+                        res = "корней нет"
+                    } else if (this.#c == 0) {
+                        x1 = 0
+                        x2 = -(this.#b / this.#a)
+                        res = `${x1}; ${x2}`
+                    } else if (this.#b != 0 && this.#c != 0) {
+                        D = this.#b ** 2 - 4 * this.#a * this.#c
+                        if (D > 0) {
+                            x1 = (-this.#b + D ** 0.5) / (2 * this.#a)
+                            x2 = (-this.#b - D ** 0.5) / (2 * this.#a)
+                            res = `${x1}; ${x2}`
+                        } else if (D == 0) {
+                            x = (-this.#b + D ** 0.5) / (2 * this.#a)
+                            res = `${x}`
+                        } else {
+                            res = 'корней нет'
+                        }
+                    }
+                } else if (this.#b == 0 && this.#c != 0) {
+                    res = "корней нет"
+                } else {
+                    // x = -(this.#c / this.#b)
+                    // res = `${x}`
+                    throw new ErrorType('Введено не квадратное уравнение, а линейное!')
+                }
+                return `Ответ: ${res}`;
+            } else {
+                throw new ErrorLetter('в параметрах не все числа!')
+            }
+        } catch (err) {
+            return err.myMessage
         }
     }
 
@@ -164,12 +188,7 @@ class Square
     }
 }
 
-try {
-    const squar = new Square(1, 0, -4)
-    console.log(squar.sqrt())
-} catch (err) {
-    console.log(err.myMessage)
-}
+const squar = new Square(1, 0, -4)
 
 
 
